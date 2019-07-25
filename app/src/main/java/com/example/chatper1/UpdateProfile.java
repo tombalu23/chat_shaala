ac0f105b8b82ed65697abf4dea1ee04cdc0917a5
Girish Cheedala <kiira97115@gmail.com>
2019-07-25 10:12:17 +0530

    Added Change Password Intent

--- app/src/main/java/com/example/chatper1/UpdateProfile.java
+++ app/src/main/java/com/example/chatper1/UpdateProfile.java
@@ -1,13 +1,18 @@
 package com.example.chatper1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class UpdateProfile extends AppCompatActivity {
-
+    Button change_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
+        change_password = findViewById(R.id.ChangePassword);
+        Intent intent = new Intent(UpdateProfile.this, ChangePassword.class);
+        startActivity(intent);
     }
 }
